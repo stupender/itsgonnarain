58b71192e9c315ef109a53004c02c3b44105f0c6
Stu Pender <stu1992@gmail.com>
2021-05-23 17:50:20 -0500

    text-instructions

--- itsgonnarain.js
+++ itsgonnarain.js
@@ -13,8 +13,6 @@ fetch('audio/MixPre-132.mp3')
     startLoop1(audioBuffer, -1, 1.0);
     startLoop1(audioBuffer, 1, 1.01);
     startLoop1(audioBuffer, -1, 1.0);
-
-
     // startLoop2(audioBuffer, 1, 2.02);
     // startLoop2(audioBuffer, -1, 2.0);
     startLoop2(audioBuffer, 1, 1.01);
